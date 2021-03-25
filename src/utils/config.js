53dcbec62adeb9a6adbc7309d0e307c554234bff
geekxia <448914712@qq.com>
2021-03-26 01:57:27 +0800

    3

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@
 const imgBase = 'http://3fengs.com'
 
 // const baseURL = 'http://10.36.138.28:8002'
-const baseURL = 'http://3fengs.com'
+const baseURL = 'http://oa.3fengs.com'
 
 // const imgAction = 'http://10.36.138.28:8002/api/v1/upload/img'
 
